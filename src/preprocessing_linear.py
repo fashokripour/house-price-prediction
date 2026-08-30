1f03492566198af33e1093ecb51e78f384809b34
fashokriopur <fashokriopur@gmail.com>
2026-08-30 19:31:56 +0330

    train Ridge model and add preprocess for linear model

---
+++ src/preprocessing_linear.py
@@ -0,0 +1,213 @@
+# src/preprocessing.py
+
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import (
+    OneHotEncoder,
+    OrdinalEncoder,
+    StandardScaler
+)
+
+
+numerical_features = [
+    "LotFrontage",
+    "LotArea",
+    "YearBuilt",
+    "YearRemodAdd",
+    "MasVnrArea",
+
+    "BsmtFinSF1",
+    "BsmtFinSF2",
+    "BsmtUnfSF",
+    "TotalBsmtSF",
+
+    "1stFlrSF",
+    "2ndFlrSF",
+    "LowQualFinSF",
+    "GrLivArea",
+
+    "GarageYrBlt",
+    "GarageCars",
+    "GarageArea",
+
+    "WoodDeckSF",
+    "OpenPorchSF",
+    "EnclosedPorch",
+    "3SsnPorch",
+    "ScreenPorch",
+
+    "PoolArea",
+    "MiscVal",
+
+    "MoSold",
+    "YrSold"
+]
+
+
+categorical_features = [
+    "MSZoning",
+    "Street",
+    "Alley",
+    "LandContour",
+    "Utilities",
+    "LotConfig",
+
+    "Neighborhood",
+    "Condition1",
+    "Condition2",
+
+    "BldgType",
+    "HouseStyle",
+
+    "RoofStyle",
+    "RoofMatl",
+
+    "Exterior1st",
+    "Exterior2nd",
+
+    "MasVnrType",
+
+    "Foundation",
+
+    "Heating",
+    "CentralAir",
+    "Electrical",
+
+    "Functional",
+
+    "GarageType",
+
+    "PavedDrive",
+
+    "Fence",
+    "MiscFeature",
+
+    "SaleType",
+    "SaleCondition"
+]
+
+
+ordinal_features = [
+    "OverallQual",
+    "OverallCond",
+
+    "ExterQual",
+    "ExterCond",
+
+    "BsmtQual",
+    "BsmtCond",
+    "BsmtExposure",
+    "BsmtFinType1",
+    "BsmtFinType2",
+
+    "HeatingQC",
+
+    "KitchenQual",
+
+    "FireplaceQu",
+
+    "GarageFinish",
+    "GarageQual",
+    "GarageCond",
+
+    "PoolQC",
+
+    "LotShape",
+    "LandSlope"
+]
+
+
+ordinal_categories = [
+    [1,2,3,4,5,6,7,8,9,10],
+    [1,2,3,4,5,6,7,8,9,10],
+
+    ["Po","Fa","TA","Gd","Ex"],
+    ["Po","Fa","TA","Gd","Ex"],
+
+    ["Po","Fa","TA","Gd","Ex"],
+    ["Po","Fa","TA","Gd","Ex"],
+    ["No","Mn","Av","Gd"],
+    ["Unf","LwQ","Rec","BLQ","ALQ","GLQ"],
+    ["Unf","LwQ","Rec","BLQ","ALQ","GLQ"],
+
+    ["Po","Fa","TA","Gd","Ex"],
+
+    ["Po","Fa","TA","Gd","Ex"],
+
+    ["Po","Fa","TA","Gd","Ex"],
+
+    ["Unf","RFn","Fin"],
+    ["Po","Fa","TA","Gd","Ex"],
+    ["Po","Fa","TA","Gd","Ex"],
+
+    ["Fa","TA","Gd","Ex"],
+
+    ["IR3","IR2","IR1","Reg"],
+    ["Sev","Mod","Gtl"]
+]
+
+
+numerical_pipeline = Pipeline([
+    ("imputer", SimpleImputer(strategy="median")),
+    ("scaler", StandardScaler())
+])
+
+
+categorical_transformer = Pipeline(
+    steps=[
+        (
+            "imputer",
+            SimpleImputer(strategy="most_frequent")
+        ),
+        (
+            "onehot",
+            OneHotEncoder(
+                handle_unknown="ignore"
+            )
+        )
+    ]
+)
+
+
+ordinal_transformer = Pipeline(
+    steps=[
+        (
+            "imputer",
+            SimpleImputer(strategy="most_frequent")
+        ),
+        (
+            "encoder",
+            OrdinalEncoder(
+                categories=ordinal_categories,
+                handle_unknown="use_encoded_value",
+                unknown_value=-1
+            )
+        ),
+        (
+            "scaler",
+            StandardScaler()
+        )
+    ]
+)
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        (
+            "numerical",
+            numerical_pipeline,
+            numerical_features
+        ),
+        (
+            "categorical",
+            categorical_transformer,
+            categorical_features
+        ),
+        (
+            "ordinal",
+            ordinal_transformer,
+            ordinal_features
+        )
+    ]
+)
---
+++ src/train_Ridge.py
@@ -0,0 +1,118 @@
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import Ridge
+import numpy as np
+
+from src.preprocessing_linear import preprocessor
+
+from src.utils import (
+    load_dataset,
+    prepare_features,
+    split_data,
+    calculate_metrics
+)
+
+from src.error_analysis import (
+    create_error_analysis,
+    plot_actual_prediction,
+    save_error_analysis,
+    evaluate_price_ranges
+)
+
+
+df = load_dataset()
+
+X, y = prepare_features(df)
+
+(
+    X_train,
+    X_val,
+    X_test,
+    y_train,
+    y_val,
+    y_test
+) = split_data(
+    X,
+    y
+)
+
+print("Total train:", len(y_train))
+
+train_analysis = X_train.copy()
+train_analysis["SalePrice"] = y_train
+
+print(train_analysis.shape)
+
+
+model = Pipeline(
+    steps=[
+        (
+            "preprocessor",
+            preprocessor
+        ),
+        (
+            "model",
+            Ridge(
+                alpha=100
+            )
+        )
+    ]
+)
+
+y_train_log = np.log1p(y_train)
+
+model.fit(
+    X_train,
+    y_train_log
+)
+
+val_prediction_log = model.predict(X_val)
+
+val_prediction = np.expm1(
+    val_prediction_log
+)
+
+# Validation metrics
+
+metrics = calculate_metrics(
+    y_val,
+    val_prediction
+)
+
+print("\nValidation Results")
+print("------------------")
+
+for key, value in metrics.items():
+
+    if key == "R2":
+        print(
+            f"{key}: {value:.4f}"
+        )
+
+    else:
+        print(
+            f"{key}: {value:.2f}"
+        )
+
+
+error_analysis = create_error_analysis(
+    X_val,
+    y_val,
+    val_prediction
+)
+
+save_error_analysis(
+    error_analysis,
+    "results/Ridge"
+)
+
+price_analysis = evaluate_price_ranges(
+    error_analysis
+)
+
+
+print("\nPrice Range Analysis")
+print("--------------------")
+
+print(
+    price_analysis
+)
